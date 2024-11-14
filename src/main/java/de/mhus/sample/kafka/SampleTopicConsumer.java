a2c58e2ad74f1c7f252c46359a11dcc340fa2557
Mike Hummel <mike@mhus.de>
2024-11-14 14:31:27 +0100

    feat: local kafka with kafka-ui

---
+++ src/main/java/de/mhus/sample/kafka/SampleTopicConsumer.java
@@ -0,0 +1,10 @@
+package de.mhus.sample.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+
+//@KafkaListener(topics = "sample-topic", groupId = "sample-group", containerFactory = "kafkaListenerContainerFactory")
+public class SampleTopicConsumer {
+
+
+
+}
